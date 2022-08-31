069103fef35a07759e5cc6ad611d692f4c4b78d0
yubocheng <Yubo.Cheng@RoswellPark.org>
2022-08-31 15:26:28 -0400

    Update run_Rcollectl.R

--- inst/docker/run_Rcollectl.R
+++ inst/docker/run_Rcollectl.R
@@ -2,5 +2,5 @@
 args = commandArgs(trailingOnly=TRUE)
 
 v = vignette(topic="workflow_hca_loom", package = "workflow")
-path = file.path(v$Dir, "doc", "workflow.Rmd")
+path = file.path(v$Dir, "doc", "workflow_hca_loom.Rmd")
 rmarkdown::render(path, output_dir = "/output", params = list(knitr_eval=as.logical(args[1]), fileId=args[2], sample=args[3]))
