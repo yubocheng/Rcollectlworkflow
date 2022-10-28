75ad116fe090ca3ca76e78fa6074b2f5d1da04ee
Yubo Cheng <yubo.cheng@roswellpark.org>
2022-10-28 15:15:37 -0400

    test vignette

--- inst/docker/install_Rcollectl.R
+++ inst/docker/install_Rcollectl.R
@@ -4,7 +4,7 @@
 BiocManager::install(c("ggplot2", "lubridate", "dplyr", "pheatmap", "sessioninfo", "DT", "devtools", "GenomicFeatures", "tidyr", "hca", "matrixStats", "SummarizedExperiment", "SingleCellExperiment", "rhdf5", "LoomExperiment", "ensembldb", "EnsDb.Hsapiens.v86", "scater",  "BiocParallel", "scran", "batchelor", "BiocNeighbors", "uwot", "bluster", "celldex", "SingleR", "rmarkdown", "BiocStyle", "knitcitations"))
 
 # Install Rcollectl
-BiocManager::install("yubocheng/Rcollectl@cl_timestamp")
+BiocManager::install("vjcitn/Rcollectl")
 
 # Install workflow vignette
-BiocManager::install("yubocheng/Rcollectlworkflow", build_vignettes = TRUE)
+BiocManager::install("yubocheng/Rcollectlworkflow", build_vignettes = TRUE, force = TRUE)
